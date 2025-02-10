817036637c2be8bb52ca37ad76e16db0d87a7b81
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-02-10 16:29:10 +0100

    removed install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-conda create -n ppillars python=3.8 -y
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-conda install conda-forge::gcc_linux-64=10 conda-forge::gxx_linux-64=10 -y
-conda install numba::numba -y
-conda install conda-forge::opencv -y
-pip install open3d -U
--- reinstall.sh
+++
@@ -1,6 +0,0 @@
-pip uninstall pointpillars -y
-rm -rf build
-rm -rf pointpillars.egg-info
-pip install .
-cd ../..
-python -c "from pointpillars.model import PillarLayer"
